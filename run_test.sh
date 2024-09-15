70f026c52bf046cc7038c5459c70966bf1309ae8
jel-yous <jawad.pro17@gmail.com>
2024-09-15 18:22:37 +0100

    u

--- run_test.sh
+++ run_test.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
+CYAN='\033[0;36m'
+RESET='\033[0m' # No Color
+
 # Get the current directory where the script is located
 SCRIPT_DIR="$(dirname "$(realpath "$0")")"
 
 # Check if Node.js is installed
 if command -v node >/dev/null 2>&1; then
-    echo "Node.js is not installed."
+    echo -e "${RED} ❌ Node.js is not installed. ${RESET}" >&2
     echo "Please install Node.js before running this script."
     echo "You can find installation instructions here: https://docs.npmjs.com/downloading-and-installing-node-js-and-npm"
     exit 1
