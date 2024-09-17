c44f1ccef2f3382e48375d99651a088f9e29f7d7
jel-yous <jawad.pro17@gmail.com>
2024-09-17 22:33:42 +0100

    d

--- app/update.js
+++ app/update.js
@@ -43,10 +43,16 @@ const cyan = '\x1b[36m';
     process.chdir(__dirname);
      return new Promise((resolve, reject) => {
          // Fetch latest changes from the remote
+         exec('git fetch', (fetchError) => {
+             if (fetchError) {
+                 console.error('Error fetching from remote:', fetchError);
+                 reject(fetchError)
+             }
+ 
              // Check the status to see if we're behind or ahead of the remote branch
              exec('git status -uno', (statusError, stdout) => {
                  if (statusError) {
-                     console.error('Error checking git status:');
+                     console.error('Error checking git status:', statusError);
                      reject(statusError)
                  }
                  // Parse the output of 'git status'
@@ -54,13 +60,14 @@ const cyan = '\x1b[36m';
                      console.log('Your repository is up to date.');
                  } else if (stdout.includes('Your branch is behind')) {
                      console.log('Your repository is behind the remote. Consider pulling.');
-                     reject(true);
+                     reject(true)
                  } else if (stdout.includes('Your branch is ahead')) {
                      console.log('Your repository is ahead of the remote. Consider pushing.');
                  } else {
                      console.log('Your repository status is unclear. Check manually.');
                  }
-                 resolve(true)
+                 resolve(true);
+             });
          });
      })
  
