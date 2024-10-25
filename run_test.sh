8204654eef9538bf21362ddbd85cc203b9d39a61
Fury U <35626062+jawadelyousfi17@users.noreply.github.com>
2024-10-25 22:38:59 +0100

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,28 +1,3 @@
-#!/bin/bash
-
-# Define colors
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[0;33m'
-CYAN='\033[0;36m'
-RESET='\033[0m' # No Color
-
-# Get the current directory where the script is located
-SCRIPT_DIR="$(dirname "$(realpath "$0")")"
-
-# Check if Node.js is installed
-# if command -v node >/dev/null 2>&1; then
-#     echo "❌ Node.js is not installed."
-#     echo "Please install Node.js before running this script."
-#     echo "You can find installation instructions here: https://docs.npmjs.com/downloading-and-installing-node-js-and-npm"
-#     exit 1
-# else
-#     echo "Node.js is installed."
-# fi
-
-# Construct the full path to your Node.js script
-NODE_SCRIPT="app/run.js"
-# Execute the Node.js script with any passed arguments
-node "~/libft-unit-test/app/run.js" "$@"
-
-#     const command = `gcc ${dirName}/tests/test_by_function.c ${__dirname}/tests/test_functions/libtest.a ${__dirname}/tests/libs/libmysd.a ` + libftPath +`  -fsanitize=address -o ${__dirname}/testLaunch.out `;
+SCRIPT_DIR="$(cd "$(dirname "$0")"; pwd)"
+NODE_SCRIPT="$SCRIPT_DIR/app/run.js"
+node "$NODE_SCRIPT" "$@"
